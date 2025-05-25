b404953d01232f4e72182c1bde3e07eae6d73e11
mbonikevin <k.mbonimpay@alustudent.com>
2025-05-25 13:02:35 +0200

    rain

---
+++ rain/0-rain.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def rain(walls):
+    if not walls or len(walls) < 3:
+        return 0
+
+    n = len(walls)
+    total_water = 0
+
+    left_max = [0] * n
+    right_max = [0] * n
+
+    left_max[0] = walls[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i - 1], walls[i])
+
+    right_max[-1] = walls[-1]
+    for i in range(n - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], walls[i])
+
+    for i in range(1, n - 1):
+        trapped = min(left_max[i], right_max[i]) - walls[i]
+        if trapped > 0:
+            total_water += trapped
+
+    return total_water
